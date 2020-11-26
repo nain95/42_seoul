4d47cbe080a43c65cc75b60d675961ec753c5700
Inyeop Jeon <ijeon@c7r3s5.42seoul.kr>
2020-11-26 18:05:29 +0900

    123

--- c02/ex12/ft_print_memory.c
+++ c02/ex12/ft_print_memory.c
@@ -6,48 +6,113 @@
 /*   By: ijeon <ijeon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/26 01:26:23 by ijeon             #+#    #+#             */
-/*   Updated: 2020/11/26 17:19:37 by ijeon            ###   ########.fr       */
+/*   Updated: 2020/11/26 18:04:28 by ijeon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdio.h>
 
+void	print_hex(int num)
+{
+	char hex;
+
+	if (num < 10)
+	{
+		hex = num +'0';
+	}
+	else
+	{
+		hex = num + 39 + '0';
+	}
+	write(1, &hex, 1);
+}
+
 void	print_memory(void *addr)
 {
 	int i;
 	int address[16];
-	long tmp;
+	long long_addr;
 	char print[2];
 
 	i = 0;
-	tmp = (long)addr;
-	while (tmp != 0)
+	long_addr = (long)addr;
+	while (long_addr != 0)
 	{
-		address[i++] = tmp % 16;
-		tmp /= 16;
+		address[i++] = long_addr % 16;
+		long_addr /= 16;
 	}
 	write(1, "0000000000000000", 16 - i);
 	while (i-- > 0)
 	{
-		if (address[i] < 10)
+		print_hex(address[i]);
+	}
+}
+
+char	print_hex_contents(char c)
+{
+	int tmp;
+	int i;
+	int hex[2];
+
+	i = 1;
+	hex[0] = 0;
+	tmp = c;
+	while (tmp != 0)
+	{
+		hex[i--] = tmp % 16;
+		tmp /= 16;
+	}
+	i++;
+	while (i < 2)
+	{
+		print_hex(hex[i++]);
+	}
+	return (c);
+}
+
+void	print_contents(char *contents)
+{
+	int i;
+
+	i = 0;
+	while (contents[i] != '\0')
+	{
+		if (0 <= contents[i] && contents[i] <= 31)
 		{
-			print[0] = address[i] + '0';
+			write(1, ".", 1);
 		}
 		else
 		{
-			print[0] = address[i] + '0' + 39;
+			write(1, &contents[i], 1);
 		}
-		write(1, print, 1);
+		i++;
 	}
+	write(1, "\n", 1);
 }
 
 void	*ft_print_memory(void *addr, unsigned int size)
 {
-	unsigned int tmp;
-	char *test;
-	long tee;
+	char *add;
+	int i;
+	char print[17];
 
-	print_memory(addr);
+	print[16] = '\0';
+	add = addr;
+	while (*add != '\0')
+	{
+		print_memory(add);
+		write(1,": ",2);
+		i = 0;
+		while (i < 16)	
+		{
+			print[i++] = print_hex_contents(*add++); 
+			if (i % 2 == 0)
+			{
+				write(1, " ", 1);
+			}
+		}
+		print_contents(print);
+	}
 	return (addr);
 }
--- c02/ex12/main.c
+++ c02/ex12/main.c
@@ -6,7 +6,7 @@
 /*   By: ijeon <ijeon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/26 03:10:37 by ijeon             #+#    #+#             */
-/*   Updated: 2020/11/26 15:58:08 by ijeon            ###   ########.fr       */
+/*   Updated: 2020/11/26 18:05:13 by ijeon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,6 @@ extern void *ft_print_memory(void *addr, unsigned int size);
 
 int	main(void)
 {
-	char test[100] = "asduhfa ashjad a\t\tadajsdjw \tadwio";
+	char test[100] = "Bonjour les aminches\t\t\tc est fo ";
 	ft_print_memory(test, 100);
 }
