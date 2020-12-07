16eb68637afa4e56cab4e7e98488833d4e41ddc6
Inyeop Jeon <ijeon@c10r3s5.42seoul.kr>
2020-12-07 16:30:20 +0900

    submit

---
+++ c07/ex04/ft_convert_base.c
@@ -0,0 +1,125 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_convert_base.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ijeon <ijeon@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/06 14:29:14 by ijeon             #+#    #+#             */
+/*   Updated: 2020/12/06 20:12:46 by ijeon            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int					check_base(char flag, char *base, char num)
+{
+	int i;
+	int j;
+
+	i = -1;
+	if (flag == 0)
+	{
+		while (base[++i])
+		{
+			if (base[i] == ' ' || base[i] == '\t' || base[i] == '\v' || \
+					base[i] == '\f' || base[i] == '\r' || base[i] == '\n')
+				return (0);
+			j = i;
+			while (base[j++])
+				if (base[i] == base[j])
+					return (0);
+		}
+		return (1);
+	}
+	while (base[++i])
+		if (base[i] == num)
+			return (i);
+	return (-1);
+}
+
+int					ft_len(char *str)
+{
+	int count;
+
+	count = 0;
+	while (str[count])
+		count++;
+	return (count);
+}
+
+unsigned int		converter(char *str, char *base)
+{
+	int				i;
+	int				len_base;
+	int				num;
+	unsigned int	answer;
+
+	num = 1;
+	answer = 0;
+	i = 0;
+	while (str[i])
+	{
+		if (check_base(1, base, str[i]) == -1)
+			break ;
+		i++;
+	}
+	len_base = ft_len(base);
+	i--;
+	while (i >= 0)
+	{
+		answer += check_base(1, base, str[i]) * num;
+		num *= len_base;
+		i--;
+	}
+	return (answer);
+}
+
+char				*converter2(unsigned int num, int flag, char *base)
+{
+	char	answer[34];
+	char	tmp[34];
+	int		i;
+	int		j;
+	char	*a;
+
+	i = 0;
+	j = 0;
+	if (flag == -1)
+	{
+		answer[i++] = '-';
+	}
+	while (num != 0)
+	{
+		tmp[j++] = base[num % ft_len(base)];
+		num /= ft_len(base);
+	}
+	j--;
+	while (j >= 0)
+		answer[i++] = tmp[j--];
+	answer[i] = '\0';
+	a = answer;
+	return (a);
+}
+
+char				*ft_convert_base(char *nbr, char *base_from, char *base_to)
+{
+	char			*nb;
+	char			*answer;
+	int				flag;
+
+	nb = nbr;
+	flag = 1;
+	if (check_base(0, base_from, ' ') == 0 || check_base(0, base_to, ' ') == 0)
+		return (0);
+	while (*nb == ' ' || *nb == '\t' || *nb == '\n' || *nb == '\v' || \
+			*nb == '\r' || *nb == '\f')
+		nb++;
+	while (*nb == '+' || *nb == '-')
+	{
+		if (*nb++ == '-')
+			flag *= -1;
+	}
+	answer = converter2(converter(nb, base_from), flag, base_to);
+	return (answer);
+}
--- c07/ex05/ft_split.c
+++ c07/ex05/ft_split.c
@@ -1,30 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ijeon <ijeon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/12/04 23:57:37 by ijeon             #+#    #+#             */
-/*   Updated: 2020/12/06 19:35:45 by ijeon            ###   ########.fr       */
+/*   Created: 2020/12/06 19:36:28 by ijeon             #+#    #+#             */
+/*   Updated: 2020/12/06 19:36:35 by ijeon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
-
-char	*ft_strdup(char *src)
-{
-	char			*answer;
-	unsigned long	count;
-
-	len = 0;
-	while (src[count])
-		count++;
-	answer = (char *)malloc(sizeof(char) * (count + 1));
-	answer[count] = '\0';
-	while (--count >= 0)
-	{
-		answer[count] = src[count];
-	}
-	return (answer);
-}
