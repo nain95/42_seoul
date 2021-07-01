8aeb35ee1b2072963ba310ae839c7843e0c7a9df
in_yeop <najio95@gmail.com>
2021-07-01 18:01:25 +0900

    push_swap

--- push_swap/tester.sh
+++ push_swap/tester.sh
@@ -1,5 +1,10 @@
 ARG=`ruby -e "puts (0..100).to_a.shuffle.join(' ')"`
-./push_swap $ARG
+#./push_swap $ARG
+./push_swap $ARG | ./checker_Mac $ARG
+./push_swap $ARG | ./checker $ARG
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..500).to_a.shuffle.join(' ')"`
+#./push_swap $ARG
 ./push_swap $ARG | ./checker_Mac $ARG
 ./push_swap $ARG | ./checker $ARG
 ./push_swap $ARG | wc -l
